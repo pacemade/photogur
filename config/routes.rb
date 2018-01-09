b854993c05fdb82c4b1c1e35e2c9488665b190f4
Danny Chang <dannychachang@gmail.com>
2018-01-09 15:25:13 -0500

    finish photogur application, add delete route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
    get 'pictures/:id/edit' => "pictures#edit"
    patch 'pictures/:id' => "pictures#update"
-  
+
+  delete 'pictures/:id' => 'pictures#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
