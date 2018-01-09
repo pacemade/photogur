5a56ce7cbe50325c1dcc07c99e09f4a24c9b4151
Danny Chang <dannychachang@gmail.com>
2018-01-09 15:14:49 -0500

    add edit and links on top of every page

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
    get 'pictures/new' => 'pictures#new'
 
    get 'pictures/:id' => 'pictures#show'
+
+   get 'pictures/:id/edit' => "pictures#edit"
+   patch 'pictures/:id' => "pictures#update"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
