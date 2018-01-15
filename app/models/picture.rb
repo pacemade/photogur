55a4539ec14b61b2c87b277edba03df4ca5a6d96
Danny Chang <dannychachang@gmail.com>
2018-01-15 16:42:12 -0500

    Include validations for artist, title, url, use partial for forms

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,9 @@
 class Picture < ApplicationRecord
 
+  validates :artist, :url, presence: true
+  validates :url, uniqueness: true
+  validates :title, length: { in: 3..20 }
+
   def self.newest_first
     Picture.order("created_at DESC")
   end
